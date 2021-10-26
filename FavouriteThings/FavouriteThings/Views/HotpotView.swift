7c44fae8273fc0d4b3c7336e43a7a01bf02944c4
Judy <jyu@lcs.on.ca>
2021-10-25 22:11:55 -0400

    Add images to assets and add three new views

---
+++ FavouriteThings/FavouriteThings/Views/HotpotView.swift
@@ -0,0 +1,24 @@
+//
+//  HotpotView.swift
+//  FavouriteThings
+//
+//  Created by Judy Yu on 2021-10-25.
+//
+
+import SwiftUI
+
+struct HotpotView: View {
+    var body: some View {
+        ScrollView {
+            VStack {
+                
+            }
+        }
+    }
+}
+
+struct HotpotView_Previews: PreviewProvider {
+    static var previews: some View {
+        HotpotView()
+    }
+}
---
+++ FavouriteThings/FavouriteThings/Views/RosesView.swift
@@ -0,0 +1,24 @@
+//
+//  RosesView.swift
+//  FavouriteThings
+//
+//  Created by Judy Yu on 2021-10-25.
+//
+
+import SwiftUI
+
+struct RosesView: View {
+    var body: some View {
+        ScrollView {
+            VStack {
+                
+            }
+        }
+    }
+}
+
+struct RosesView_Previews: PreviewProvider {
+    static var previews: some View {
+        RosesView()
+    }
+}
