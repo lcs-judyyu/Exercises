7afc340041da056c24d49660e98f3ae629ef2255
Judy <jyu@lcs.on.ca>
2021-10-28 21:54:38 -0400

    Change font, font size, and weight add an background to the text

--- LayoutExercise/LayoutExercise/Views/ThirdLayout.swift
+++ LayoutExercise/LayoutExercise/Views/ThirdLayout.swift
@@ -18,19 +18,22 @@ struct ThirdLayout: View {
                 
                 HStack(alignment: .top, spacing: 10) {
                     Text("""
-Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Habitant morbi tristique senectus et netus et. Est ante in nibh mauris cursus mattis molestie a. Aenean et tortor at risus viverra adipiscing at in tellus. Quam lacus suspendisse faucibus interdum posuere lorem ipsum dolor.
-""")
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Habitant morbi tristique senectus et netus et. Est ante in nibh mauris cursus mattis molestie a. Aenean et tortor at risus viverra adipiscing at in tellus. Quam lacus suspendisse faucibus interdum posuere lorem ipsum dolor.
+    """)
+                        .font(Font.custom("Avenir Next", size: 12).weight(.light))
                     Text("""
-Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Habitant morbi tristique senectus et netus et. Est ante in nibh mauris cursus mattis molestie a. Aenean et tortor at risus viverra adipiscing at in tellus. Quam lacus suspendisse faucibus interdum posuere lorem ipsum dolor.
-""")
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Habitant morbi tristique senectus et netus et. Est ante in nibh mauris cursus mattis molestie a. Aenean et tortor at risus viverra adipiscing at in tellus. Quam lacus suspendisse faucibus interdum posuere lorem ipsum dolor.
+    """)
+                        .font(Font.custom("Avenir Next", size: 12).weight(.light))
                 }
                 .padding(.leading, 20)
                 .padding(.trailing, 70)
-                .background(Color("DarkYellow"))
+                .background(Color("DarkYellow").opacity(0.5))
                 
                 VStack {
                     AsyncImage(url: URL(string: "https://placekitten.com/90/100"))
                 }
+                
             }
         }
         .edgesIgnoringSafeArea(.all)
