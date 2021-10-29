1666e99c5e7bbc2594fddd94b16f566ac0f39ebd
Judy <jyu@lcs.on.ca>
2021-10-28 21:13:44 -0400

    Re-indent second layout Add a new file called ThirdLayout Add ScrollView and NavigationView to the third file Move the comment of placeholder text to the third file.

--- LayoutExercise/LayoutExercise/Views/SecondLayout.swift
+++ LayoutExercise/LayoutExercise/Views/SecondLayout.swift
@@ -87,8 +87,6 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor i
     }
 }
 
-//"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Habitant morbi tristique senectus et netus et. Est ante in nibh mauris cursus mattis molestie a. Aenean et tortor at risus viverra adipiscing at in tellus. Quam lacus suspendisse faucibus interdum posuere lorem ipsum dolor. Vitae tortor condimentum lacinia quis vel eros. Leo duis ut diam quam nulla porttitor massa id neque. Quis commodo odio aenean sed adipiscing. Ac felis donec et odio. Hendrerit dolor magna eget est lorem ipsum. Purus faucibus ornare suspendisse sed nisi lacus sed. Egestas fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate. Sit amet nisl purus in mollis nunc sed id. In tellus integer feugiat scelerisque varius morbi enim. Augue lacus viverra vitae congue eu consequat ac felis donec. Vitae semper quis lectus nulla at volutpat. "
-
 struct SecondLayout_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
