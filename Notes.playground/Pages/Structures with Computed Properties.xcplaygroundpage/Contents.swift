34d45378db9882c06688533af2b73902349e6dad
Judy <jyu@lcs.on.ca>
2021-10-31 15:58:23 -0400

    Add computed properties and test result

--- Notes.playground/Pages/Structures with Computed Properties.xcplaygroundpage/Contents.swift
+++ Notes.playground/Pages/Structures with Computed Properties.xcplaygroundpage/Contents.swift
@@ -6,8 +6,27 @@ struct Cone {
     var radius: Double
     var side: Double
     var height: Double
+    
+    var surfaceAreaOfBase: Double {
+        return pi * (radius * radius)
+    }
+    var surfaceAreaOfLateralSurface: Double {
+        return pi * radius * side
+    }
+    var totalSurfaceArea: Double {
+        return surfaceAreaOfBase + surfaceAreaOfLateralSurface
+    }
+    var volume: Double {
+        return (surfaceAreaOfBase * height) / 3
+    }
 }
 
+let oneCone = Cone(name: "One Cone", pi: Double.pi, radius: 4.5, side: 8, height: 6)
+
+oneCone.surfaceAreaOfBase
+oneCone.surfaceAreaOfLateralSurface
+oneCone.totalSurfaceArea
+oneCone.volume
 
 
-Double.pi
+
