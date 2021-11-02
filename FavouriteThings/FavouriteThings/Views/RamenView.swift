c0b523a58cfd045f2e8e9aced2fa7105efc0e0c0
Judy <jyu@lcs.on.ca>
2021-11-02 14:33:55 -0400

    add photo captions and credit to the ramen page.

--- FavouriteThings/FavouriteThings/Views/RamenView.swift
+++ FavouriteThings/FavouriteThings/Views/RamenView.swift
@@ -11,12 +11,26 @@ struct RamenView: View {
     var body: some View {
         ScrollView {
             VStack {
-                Image("Ramen")
-                    .resizable()
-                    .scaledToFit()
+                VStack(alignment: .leading) {
+                    Image("Ramen")
+                        .resizable()
+                        .scaledToFit()
                     .padding(20)
+                    
+                    VStack(alignment: .leading){
+                        Text ("A bowl of ramne that looks delicious")
+                            .font(.caption)
+                        Text ("Online resource")
+
+                    }
+                    .padding(.horizontal, 15)
+                    .font(.caption)
+                    .Italic
+                }
                 
-                Text("Ramen is another dish I really like.\n\nLorem ipsum dolor sit amet, Dui sapien eget mi proin sed libero enim. Gravida rutrum quisque non tellus. Tristique senectus et netus et malesuada fames ac turpis. Lorem ipsum dolor sit amet consectetur adipiscing. Tellus integer feugiat scelerisque varius morbi enim nunc faucibus. Fringilla urna porttitor rhoncus dolor purus non enim praesent elementum. Nisi porta lorem mollis aliquam ut porttitor leo a.")
+                Text("""
+Ramen is another dish I really like.\n\nLorem ipsum dolor sit amet, Dui sapien eget mi proin sed libero enim. Gravida rutrum quisque non tellus. Tristique senectus et netus et malesuada fames ac turpis. Lorem ipsum dolor sit amet consectetur adipiscing. Tellus integer feugiat scelerisque varius morbi enim nunc faucibus. Fringilla urna porttitor rhoncus dolor purus non enim praesent elementum. Nisi porta lorem mollis aliquam ut porttitor leo a.
+""")
                     .frame(width: 270, height: 800, alignment: .top)
                     .padding(20)
                 
